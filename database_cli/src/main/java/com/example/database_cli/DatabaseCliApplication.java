0b0b28641133550d144acc89d20b28820125ac40
以空\白峰楠 <>
2025-06-16 15:40:57 +0800

    first commit

---
+++ database_cli/src/main/java/com/example/database_cli/DatabaseCliApplication.java
@@ -0,0 +1,13 @@
+package com.example.database_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseCliApplication.class, args);
+	}
+
+}
---
+++ database_cli/src/test/java/com/example/database_cli/DatabaseCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.database_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
