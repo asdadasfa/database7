4e9d1afb04ade19edf53e567edaa3e3bc52f8858
以空\白峰楠 <>
2025-06-18 16:08:30 +0800

    文件格式修改

---
+++ 后端服务代码/database_cli/src/main/java/com/example/database_cli/control/goodsControl.java
@@ -0,0 +1,11 @@
+package com.example.database_cli.control;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/goods")
+public class goodsControl {
+}
